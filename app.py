2418f0514d2a08902d4021593f1a8d0083e3f12f
Sahil000-3 <mikumohankudo@gmail.com>
2026-04-28 13:04:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Internal Developer Platform is Running!"
+    return "🔥 CI/CD is Working!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
